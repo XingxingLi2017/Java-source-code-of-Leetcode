dcb5d48d630077493e4f0ca66c1bf94b4ee42e18
Xingxing Li <xingxingli2017@gmail.com>
2017-03-16 02:08:00 -0400

    Container with Most Water

---
+++ 12-Container with most water-11.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ * **/
+class Solution {
+    public int maxArea(int[] height) {
+        int low = 0;
+        int high = height.length-1;
+        int max = 0;
+        int bottom, tempHeight,tempArea;
+        while(low < high)
+        {
+        	bottom = high-low;
+        	tempHeight = Math.min(height[low], height[high]);
+        	tempArea = bottom*tempHeight;
+        	max = Math.max(max, tempArea);
+        	if(height[low] < height[high]) low++;
+        	else {
+				high--;
+			}
+        	
+        }
+        return max;
+    }
+}
+public class Ok {
+	 public static void main(String[] args){
+		Solution aSolution = new Solution();
+		int[] nums = {2,3,10,5,7,8,9};
+		System.out.print("n = :");
+		for(int i = 0 ; i < nums.length ; i++)
+		{
+			System.out.print(nums[i]+" ");
+		}
+		System.out.println("");
+		
+		
+		//print
+		System.out.println(aSolution.maxArea(nums));
+		
+		 
+   }
+}
